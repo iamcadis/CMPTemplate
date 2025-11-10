b12893d9fa298095f1a7d61fc1ec58f24bdb1da8
Cadis Hangoluan <hangoluancadis@gmail.com>
2025-11-10 21:58:13 +0700

    refactor: Remove inline from `onLoad` Flow extension
    The `inline` keyword has been removed from the `onLoad` extension function for `Flow`. This change simplifies the function signature as the `crossinline` modifier for the `action` parameter is no longer necessary.

--- core/src/commonMain/kotlin/com/core/utils/Flows.kt
+++ core/src/commonMain/kotlin/com/core/utils/Flows.kt
@@ -4,6 +4,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
 
-inline fun <T> Flow<T>.onLoad(crossinline action: (Boolean) -> Unit): Flow<T> {
+fun <T> Flow<T>.onLoad(action: (Boolean) -> Unit): Flow<T> {
     return this.onStart { action(true) }.onCompletion { action(false) }
 }
