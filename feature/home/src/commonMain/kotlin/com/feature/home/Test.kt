f5aa3ee94c472918bf48d7c702e138272343fbfd
Cadis Hangoluan <hangoluancadis@gmail.com>
2025-11-18 14:27:58 +0700

    refactor: Update test screen and rename utility file
    This commit updates the test screen and renames a utility file for better clarity.
    
    In the `Test.kt` screen, the `pageTitle` resource has been changed from `title` to `testing`. The empty `floatingActionButton` has been removed, and the `confirmOnLeave` parameter has been enabled by setting it to `true`.
    
    Additionally, the `MutableStateMap.kt` file has been renamed to `MutableStateMapSaver.kt` to more accurately reflect its purpose.

--- feature/home/src/commonMain/kotlin/com/feature/home/Test.kt
+++ feature/home/src/commonMain/kotlin/com/feature/home/Test.kt
@@ -9,7 +9,7 @@ import kotlinx.serialization.Serializable
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import template.feature.home.generated.resources.Res
-import template.feature.home.generated.resources.title
+import template.feature.home.generated.resources.testing
 
 object Test {
     @Serializable
@@ -19,10 +19,8 @@ object Test {
     fun Screen(data: String) {
         BaseScreen(
             viewModel = koinViewModel<HomeViewModel>(),
-            pageTitle = stringResource(Res.string.title),
-            floatingActionButton = {
-
-            }
+            pageTitle = stringResource(Res.string.testing),
+            confirmOnLeave = true,
         ) { state, _ ->
             Column {
                 Text("Current: ${state.currentDate}")
